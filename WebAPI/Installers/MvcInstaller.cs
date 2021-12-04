4aad17d695e890adaabef73421d85f3b86a34b8b
HiMatterhorn <wyp4tomasz@gmail.com>
2021-12-04 17:47:20 +0100

    Dependency Injection

---
+++ WebAPI/Installers/MvcInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Application;
+using Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebAPI.Installers
+{
+    public class MvcInstaller : IInstaller
+    {
+        public void InstallSerServices(IServiceCollection services, IConfiguration Configuration)
+        {
+
+            services.AddApplication();
+            services.AddInfrastructure();
+            services.AddControllers();
+        }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,14 +1,9 @@
-using Application.Interfaces;
-using Application.Mappings;
-using Application.Services;
-using Domain.Interfaces;
-using Infrastructure.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
+using WebAPI.Installers;
 
 namespace WebAPI
 {
@@ -24,17 +19,8 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IPostRepository, PostRepository>();
-            services.AddScoped<IPostService, PostService>();
+            services.InstallServicesInAssembly(Configuration);
 
-            services.AddSingleton(AutoMapperConfig.Initialize());
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.EnableAnnotations();
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
